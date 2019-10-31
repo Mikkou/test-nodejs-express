742b9bf9bedfb3297c1c7911b0f737c13ec36363
mikkou <developer.0112358@gmail.com>
2019-10-31 17:26:40 +0300

    made pages: /admin/login, /admin

---
+++ pages/admin/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <b-container>
+    <b-input-group
+      size="lg"
+      class="mb-3"
+      prepend="About me"
+    >
+      <b-form-input></b-form-input>
+      <b-input-group-append>
+        <b-button size="sm" text="Button" variant="success">Сохранить</b-button>
+      </b-input-group-append>
+    </b-input-group>
+  </b-container>
+</template>
+
+<script>
+  export default {
+    layout: 'admin'
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/admin/login.vue
@@ -0,0 +1,59 @@
+<template>
+  <b-container>
+    <b-form @submit="onSubmit">
+      <b-form-group
+        id="input-group-1"
+        label="Login"
+        label-for="input-1"
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.login"
+          type="text"
+          required
+          placeholder="Enter login"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group
+        id="input-group-1"
+        label="Enter password"
+        label-for="input-1"
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.password"
+          type="password"
+          required
+          placeholder="Enter password"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+    </b-form>
+  </b-container>
+</template>
+
+<script>
+  export default {
+    layout: 'admin',
+    data() {
+      return {
+        form: {
+          login: '',
+          password: ''
+        }
+      }
+    },
+    methods: {
+      onSubmit(evt) {
+        evt.preventDefault()
+        alert(JSON.stringify(this.form))
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
